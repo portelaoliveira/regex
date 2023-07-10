2c38cbae9f2f0d7329e0202e73c16f9d163aa597
Danilo Portela <daniloportela97@gmail.com>
2023-07-10 15:33:46 -0300

    Operadores - Regex.

--- regex_1.py
+++ regex_1.py
@@ -1,3 +1,5 @@
+import re
+
 texto = """
 Bom dia,
 
@@ -39,3 +41,74 @@ https://www.wikipedia.org/
 www.bcb.gov.br/
 
 Qualquer dúvida estamos à disposição"""
+
+# Quantas comidas
+
+# padrao = re.compile("comida")
+# resultado = re.findall(padrao, texto)
+# print(resultado, f"- Quantas comidas: {len(resultado)}")
+
+
+# Quantas bebidas
+
+# padrao = re.compile("bebida")
+# resultado = re.findall(padrao, texto)
+# print(resultado, f"- Quantas bebidas: {len(resultado)}")
+
+# Extrair números
+
+# padrao = re.compile("\d+,?\d+|\d+")
+# padrao = re.compile("\d+,?\d*")
+# resultado = re.findall(padrao, texto)
+# print(resultado, f"- Quantos números: {len(resultado)}")
+
+# Extrair quantos ml(do 1 item)
+
+# padrao = re.compile(r"\(.+\)")
+# resultado = re.search(padrao, texto)
+# print(resultado)
+
+# padrao = re.compile(r"\(\d+\s\w+\)")
+# resultado = re.search(padrao, texto)
+# print(resultado)
+
+# Preço do 1 item
+
+# padrao = re.compile(r"R\$\d+,?\d+")
+# resultado = re.search(padrao, texto)
+# print(resultado)
+
+# Todos os preço 
+
+# padrao = re.compile(r"R\$\s?\d+,?\d*")
+# resultado = re.findall(padrao, texto)
+# print(resultado)
+
+# padrao = re.compile(r"R\$\s?(\d+,?\d*)")
+# resultado = re.findall(padrao, texto)
+# print(resultado)
+
+
+# Quantidades dos itens
+
+# padrao = re.compile(r"\(.+\)")
+# resultado = re.findall(padrao, texto)
+# print(resultado)
+
+# padrao = re.compile(r"\((.+)\)")
+# resultado = re.findall(padrao, texto)
+# print(resultado)
+
+# Links
+
+# padrao = re.compile(r"http.+\s")
+# resultado1 = re.findall(padrao, texto2)
+# resultado2 = re.finditer(padrao, texto2)
+# print(resultado1)
+# for item in resultado2:
+#     print(item)
+
+padrao = re.compile(r"(https?://)?(www\.)?\w+\.(com\.br|com|org|gov\.br)")
+resultado = re.finditer(padrao, texto2)
+for item in resultado:
+    print(item.group(0))
